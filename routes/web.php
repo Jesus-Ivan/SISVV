3f87d8d8d2f39d2b48ef7a7eb64cbabfcc45da45
Lui24ngel <romeroluis338@gmail.com>
2024-03-31 11:18:35 -0600

    dropdown del menu inicio para almacen

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,33 @@ Route::view('profile', 'profile')
 Route::view('recepcion', 'recepcion')
     ->middleware(['auth'])
     ->name('recepcion');
-Route::view('almacen', 'almacen')
-    ->middleware(['auth'])
-    ->name('almacen');
+
+Route::prefix('almacen')->middleware(['auth'])->group(function () {
+    Route::view('/','almacen.index')->name('almacen');
+    
+    Route::view('articulos','almacen.articulos')->name('almacen.articulos');
+    Route::view('asignar','almacen.asignar')->name('almacen.asignar');
+    Route::view('proveedores','almacen.proveedores')->name('almacen.proveedores');
+    Route::view('familias','almacen.familias')->name('almacen.familias');
+    Route::view('categorias','almacen.categorias')->name('almacen.categorias');
+    Route::view('unidades','almacen.unidades')->name('almacen.unidades');
+
+    Route::prefix('entradas')->group(function(){
+        Route::view('/','almacen.Entradas.entradas')->name('almacen.entradas');
+        Route::view('historial','almacen.Entradas.historial')->name('almacen.entradas.historial');
+    });
+    Route::prefix('salidas')->group(function(){
+        Route::view('/','almacen.Salidas.salidas')->name('almacen.salidas');
+        Route::view('nueva','almacen.Salidas.nueva-salida')->name('almacen.salidas.nueva');
+    });
+    Route::prefix('traspasos')->group(function(){
+        Route::view('/','almacen.Traspasos.traspasos')->name('almacen.traspasos');
+        Route::view('nuevo','almacen.Traspasos.nuevo-traspaso')->name('almacen.traspasos.nuevo');
+        Route::view('historial','almacen.Traspasos.historial')->name('almacen.traspasos.historial');
+    });
+});
+
+
 Route::view('cocina', 'cocina')
     ->middleware(['auth'])
     ->name('cocina');

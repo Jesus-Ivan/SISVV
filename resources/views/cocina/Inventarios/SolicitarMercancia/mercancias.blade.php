1e3c5e1fe5c51678cce02d4011795b8953fcd884
Jesus-Ivan <jesivanprz13@gmail.com>
2024-04-09 14:04:51 -0600

    Cocina, inventarios, frontend

---
+++ resources/views/cocina/Inventarios/SolicitarMercancia/mercancias.blade.php
@@ -0,0 +1,152 @@
+<x-app-layout>
+    {{-- Sub barra de navegacion --}}
+    <x-slot name="header">
+        @include('cocina.nav')
+    </x-slot>
+    {{-- Contenido --}}
+    <div class="p-2">
+        <!--Titulo -->
+        <div class="inline-flex flex-grow py-2">
+            <a type="button" href="{{ route('cocina.mercancias.nueva-soli') }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd"
+                        d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                        clip-rule="evenodd" />
+                </svg>
+            </a>
+            <h4 class="flex items-center ms-2 text-2xl font-bold dark:text-white">Solicitar mercancia</h4>
+        </div>
+        {{-- Tabla --}}
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            FOLIO
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            FECHA
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ORIGEN
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            DESTINO
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            OBSERVACIONES
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ESTADO
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ACCIONES
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            1234
+                        </td>
+                        <td class="px-6 py-4">
+                            12/12/2023
+                        </td>
+                        <td class="px-6 py-4">
+                            ALMACÉN
+                        </td>
+                        <td class="px-6 py-4">
+                            CAFETERÍA
+                        </td>
+                        <td class="px-6 py-4">
+                            REPARTO DE MERCANCIA REALIZADO PARA EL PUNTO DE VENTA
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <div class="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Aprobado
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex">
+                                <button data-modal-target="modal-productos" data-modal-toggle="modal-productos"
+                                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2 text-center inline-flex items-blue me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+                                    <svg class="w-5 h-5 aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd"
+                                            d="M8 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1h2a2 2 0 0 1 2 2v15a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h2Zm6 1h-4v2H9a1 1 0 0 0 0 2h6a1 1 0 1 0 0-2h-1V4Zm-6 8a1 1 0 0 1 1-1h6a1 1 0 1 1 0 2H9a1 1 0 0 1-1-1Zm1 3a1 1 0 1 0 0 2h6a1 1 0 1 0 0-2H9Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                    Ver Lista
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!--Modal detalles de solicitud -->
+    <div id="modal-productos" tabindex="-1" aria-hidden="true"
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-2xl max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Detalles de la solicitud
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="modal-productos">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-4 md:p-5 space-y-4">
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <!--table-->
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        CODIGO
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        DESCRIPCION
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        CANTIDAD
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        1023
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        JITOMATE
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        2.125
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

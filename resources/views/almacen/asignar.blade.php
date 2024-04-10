bed0838d716a35b9e2d6c2925529a3ef8f65c988
Lui24ngel <romeroluis338@gmail.com>
2024-04-10 14:44:45 -0600

    apartado de asignar articulos en almacen

--- resources/views/almacen/asignar.blade.php
+++ resources/views/almacen/asignar.blade.php
@@ -3,9 +3,146 @@
     <x-slot name="header">
         @include('almacen.nav')
     </x-slot>
-    
+
     {{-- Contenido --}}
-    <div>
-        TABLA REPARTO
+    <div class="container py-5">
+        <h4 class="ms-3 text-2xl font-bold dark:text-white">Asignar productos</h4>
+    </div>
+
+    {{-- Tabla --}}
+    <div class="ms-3 mx-3">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
+                <label for="table-search" class="sr-only">Buscar</label>
+                <div class="relative">
+                    <div
+                        class="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <input type="text" id="table-search"
+                        class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Buscar">
+                </div>
+            </div>
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            CÓDIGO
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            FAMILIA
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            CATEGORÍA
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            DESCRIPCIÓN
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            UNIDAD
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            BAR
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            BARRA
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            CADDIE
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            CAFETERÍA
+                        </th>
+                        <th scope="col" class="px-4 py-3">
+                            COCINA
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4">
+                            1234
+                        </td>
+                        <td class="px-6 py-4">
+                            BEBIDAS
+                        </td>
+                        <td class="px-6 py-4">
+                            MINERAL
+                        </td>
+                        <td class="px-6 py-4">
+                            AGUA MINERAL PEÑAFIEL 600ML
+                        </td>
+                        <td class="px-6 py-4">
+                            PIEZA
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-bar" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="checkbox-bar" class="sr-only"></label>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-barra" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="checkbox-barra" class="sr-only"></label>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-caddie" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="checkbox-caddie" class="sr-only"></label>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-cafeteria" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="checkbox-cafeteria" class="sr-only"></label>
+                            </div>
+                        </td>
+                        <td class="px-6 py-4">
+                            <div class="flex items-center">
+                                <input id="checkbox-cocina" type="checkbox"
+                                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <label for="checkbox-cocina" class="sr-only"></label>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <hr class="h-px my-4 bg-gray-300 border-0 dark:bg-gray-700">
+
+        {{-- Botones de accion --}}
+        <div class="inline-flex flex-grow mt-2">
+            <a type="button" href="{{ route('almacen') }}"
+                class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                <svg class="w-5 h-5 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                    height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M5 12h14M5 12l4-4m-4 4 4 4" />
+                </svg>Regresar
+            </a>
+            <button type="button"
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                <svg class="w-5 h-5 me-2" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                    viewBox="0 0 24 24">
+                    <path
+                        d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z" />
+                </svg>Guardar Cambios
+            </button>
+        </div>
     </div>
 </x-app-layout>

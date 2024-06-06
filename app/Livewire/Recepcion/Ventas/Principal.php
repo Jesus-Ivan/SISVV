8a8939594e246f0801e3b78742f3e24bcd037d90
Jesus-Ivan <jesivanprz13@gmail.com>
2024-06-05 20:59:28 -0600

    fecha-reportes-cob-ven

--- app/Livewire/Recepcion/Ventas/Principal.php
+++ app/Livewire/Recepcion/Ventas/Principal.php
@@ -10,12 +10,25 @@ use Livewire\WithPagination;
 class Principal extends Component
 {
     use WithPagination;
+    public $search;
+    public $fecha;
+
+    public function mount(){
+        $this->fecha = now()->toDateString();
+    }
+
+    public function buscar (){
+        $this->resetPage();
+    }
+
 
     #[Computed()]
     public function ventasHoy()
     {
-        //return Venta::whereDate('fecha_apertura', now()->format('Y-m-d'))->get();
-        return Venta::orderby('fecha_apertura','desc')->paginate(10);
+        return Venta::whereDate('fecha_apertura',$this->fecha)
+            ->whereAny(['id_socio','nombre'], 'like', '%' . $this->search . '%')
+            ->orderby('fecha_apertura', 'desc')
+            ->paginate(10);
     }
 
     public function render()

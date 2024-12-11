49ceadfd487dad7af7fbc73ac81aef78ed0c3314
Jesus-Ivan <jesivanprz13@gmail.com>
2024-12-11 10:28:31 -0600

    historial compras

--- resources/views/almacen/Entradas/historial.blade.php
+++ resources/views/almacen/Entradas/historial.blade.php
@@ -8,7 +8,7 @@
     <div class="container py-5">
         <div class="flex ms-3">
             <div class="inline-flex flex-grow">
-                <h4 class="flex items-center ms-2 text-2xl font-bold dark:text-white">Historial de entradas</h4>
+                <h4 class="flex items-center ms-2 text-2xl font-bold dark:text-white">HISTORIAL DE ENTRADAS</h4>
             </div>
         </div>
     </div>

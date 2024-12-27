08f6cda6009311652b7f447973c9c1c7ab91f8f1
Lui24ngel <romeroluis338@gmail.com>
2024-12-27 14:41:56 -0600

    mermas todod incompleto xd

---
+++ app/Livewire/Almacen/Mermas/Principal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Almacen\Mermas;
+
+use Livewire\Component;
+
+class Principal extends Component
+{
+    public function render()
+    {
+        return view('livewire.almacen.mermas.principal');
+    }
+}
---
+++ resources/views/livewire/almacen/mermas/principal.blade.php
@@ -0,0 +1,3 @@
+<div class="ms-3 mx-3">
+    @include('livewire.almacen.mermas.modal-mermas')
+</div>

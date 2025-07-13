d8ecc42f374c7b3704770986b9ff20b30ec5f0eb
Lui24ngel <romeroluis338@gmail.com>
2025-07-12 19:12:12 -0600

    facturas y detalles

--- app/Models/DetallesFacturas.php
+++ app/Models/DetallesFacturas.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class DetallesFacturas extends Model
 {
@@ -11,7 +12,17 @@ class DetallesFacturas extends Model
     //Nombre de la tabla
     protected $table = 'detalles_facturas';
     //Propiedades restringidas para asignacion masiva
-    protected $guarded = ['id'];
+    protected $guarded = ['folio'];
     //Clave primaria
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'folio';
+
+    public function proveedores(): BelongsTo
+    {
+        return $this->belongsTo(Proveedor::class, 'id_proveedor', 'id');
+    }
+
+    public function presentacion(): BelongsTo
+    {
+        return $this->belongsTo(Presentacion::class, 'clave_presentacion', 'clave');
+    }
 }
--- database/migrations/2025_06_20_110359_create_facturas_table.php
+++ database/migrations/2025_06_20_110359_create_facturas_table.php
@@ -15,11 +15,13 @@ return new class extends Migration
             $table->integer('folio')->autoIncrement()->unsigned();
             $table->date('fecha_compra');
             $table->date('fecha_vencimiento')->nullable();
-            $table->integer('folio_entrada');
+            $table->integer('folio_entrada')->nullable();
             $table->integer('id_proveedor');
+            $table->decimal('subtotal', 10, 2);
+            $table->decimal('iva', 10, 2);
+            $table->decimal('total', 10, 2);
             $table->string('cuenta_contable', 255);
-            $table->string('referencia', 255)->nullable();
-            $table->string('folio_revision', 255)->nullable();
+            $table->string('folio_remision', 255)->nullable();
             $table->string('user_name', 255);
             $table->string('observaciones', 255)->nullable();
             $table->timestamps();

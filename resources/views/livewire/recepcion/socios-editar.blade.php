66fc5c0b5028d7eccfd47fdf8ed4e6f57213d24c
Jesus-Ivan <jesivanprz13@gmail.com>
2025-07-31 19:06:20 -0600

    correccion numero integrante

--- resources/views/livewire/recepcion/socios-editar.blade.php
+++ resources/views/livewire/recepcion/socios-editar.blade.php
@@ -353,7 +353,7 @@
                         <div class="w-full">
                             <label for="tel_integrante"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Celular</label>
-                            <input type="tel" id="tel_integrante" wire:model="formSocio.tel_integrante"
+                            <input type="tel" id="tel_integrante" wire:model="form.tel_integrante"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required />
                         </div>

703d772eaf9022c74621f949de6038931ede967b
Jesus-Ivan <jesivanprz13@gmail.com>
2025-11-26 13:39:16 -0600

    historial transformaciones, listo

--- app/Livewire/Almacen/Produccion/HistorialProduccion.php
+++ app/Livewire/Almacen/Produccion/HistorialProduccion.php
@@ -3,24 +3,36 @@
 namespace App\Livewire\Almacen\Produccion;
 
 use App\Models\DetalleTransformacion;
+use Carbon\Carbon;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class HistorialProduccion extends Component
 {
     use WithPagination;
-    public $mes_busqueda, $search = '';
+    public $mes_busqueda;
+    public $selected_insumo = null;
 
     public function mount()
     {
         $this->mes_busqueda = now()->format('Y-m');
     }
 
-    public function actualizar()
+    public function buscar()
     {
         $this->resetPage();
+        $this->reset('selected_insumo');
     }
+
+    #[On('selected-insumo')]
+    public function onSelectedInsumo($clave)
+    {
+        $this->selected_insumo = $clave;
+    }
+
+
     #[Computed()]
     public function producciones()
     {
@@ -32,12 +44,25 @@ class HistorialProduccion extends Component
             'rendimiento',
             'total_elaborado'
         ];
+
+        //Parsear la fecha seleccionada
+        $fecha = Carbon::parse($this->mes_busqueda);
+        //Definir consulta base
         $resultados = DetalleTransformacion::query()
             ->with('insumoElaborado', 'transformacion.origen', 'transformacion.destino')
+            ->whereHas('transformacion', function ($query) use ($fecha) {
+                $query->whereMonth('fecha_existencias', $fecha->month)
+                    ->whereYear('fecha_existencias', $fecha->year);
+            })
             ->select($columns)
-            ->where('clave_insumo_elaborado', 'like', '%' . $this->search . '%')
             ->groupBy($columns);
-        return $resultados->paginate(10);
+
+        //Si hay seleccionado un insumo, agregrar la condicion extra
+        if ($this->selected_insumo) {
+            $resultados->where('clave_insumo_elaborado', $this->selected_insumo);
+        }
+
+        return $resultados->paginate(15);
     }
     public function render()
     {
